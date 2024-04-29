dc91a87fe95423760d71371301782c734eb51c78
tekkac <tarik@carbonable.io>
2024-04-29 13:39:23 +0200

    🐛 Add Suspense for build

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { StarknetProvider } from "./components/StarknetProvider";
+import { Suspense } from 'react'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-neutral-800 text-neutral-100 min-h-screen`}>
         <main className="mx-auto mt-8 pb-24 px-4 max-w-7xl">
-          <StarknetProvider rpcApiKey={rpcApiKey}>
-            {children}
-          </StarknetProvider>
+          <Suspense>
+            <StarknetProvider rpcApiKey={rpcApiKey}>
+              {children}
+            </StarknetProvider>
+          </Suspense>
         </main>
       </body>
     </html>
