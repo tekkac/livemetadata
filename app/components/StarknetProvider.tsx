4c0c56da889e07465aed82bbf7539caea7147762
tekkac <tarik@carbonable.io>
2024-05-02 17:53:38 +0200

    👽️ change RPC provider

--- app/components/StarknetProvider.tsx
+++ app/components/StarknetProvider.tsx
@@ -1,11 +1,17 @@
 "use client"
-import { StarknetConfig, argent, braavos, nethermindProvider, publicProvider } from "@starknet-react/core";
+import { StarknetConfig, argent, braavos, nethermindProvider, jsonRpcProvider } from "@starknet-react/core";
 import { mainnet } from "@starknet-react/chains";
 import { useMemo } from "react";
 
 export function StarknetProvider({ children, rpcApiKey }: { children: React.ReactNode, rpcApiKey: string }) {
-    // const provider = nethermindProvider({ apiKey: rpcApiKey });
-    const provider = publicProvider();
+    const provider = jsonRpcProvider({
+        rpc: (chain) => {
+            return {
+                nodeUrl: "https://starknet-mainnet.public.blastapi.io/rpc/v0_7"
+            }
+        }
+    });
+
     const connectors = useMemo(() => [braavos(), argent()], []);
 
     return (
